aa2d37f7e843c0b9ed24bfc61259e44ac61fb14c
TimeSentinel <lance.stubblefield@gmail.com>
2025-01-27 15:24:14 -0800

    adding routes

--- src/App.svelte
+++ src/App.svelte
@@ -11,40 +11,66 @@
     <div class="container">
         <div class="topRow">
             <div class="leftCol">
-                <div class="screen1"></div>
+                <a href="/" aria-label="1">
+                    <div class="screen1"></div>
+                </a>
+                <span class="cellTag">services</span>
             </div>
             <div class="centerCol">
-                <div class="screen2"></div>
+                <a href="/" aria-label="2">
+                    <div class="screen2"></div>
+                </a>
+                <span class="cellTag">technologies</span>
             </div>
             <div class="rightCol">
-                <div class="screen3"></div>
+                <a href="/" aria-label="3">
+                    <div class="screen3"></div>
+                </a>
+                <span class="cellTag">case studies</span>
             </div>
         </div>
         <div class="middleRow">
             <div class="leftCol">
-                <div class="screen4"></div>
+                <a href="https://lance.sol3.one" aria-label="Lance's React Portfolio">
+                    <div class="screen4"></div>
+                </a>
+                <span class="cellTag">portfolio</span>
             </div>
             <div class="mainFocus">
-                <div class="screen5">
+                <a href="/" aria-label="5">
+                    <div class="screen5">
 
-                </div>
+                    </div>
+                </a>
                 <div class="focus">
                     <h3>design by Lance</h3>
                 </div>
             </div>
             <div class="rightCol">
-                <div class="screen6"></div>
+                <a href="/" aria-label="6">
+                    <div class="screen6"></div>
+                </a>
+                <span class="cellTag">tech samples</span>
             </div>
         </div>
         <div class="bottomRow">
             <div class="leftCol">
-                <div class="screen7"></div>
+                <a href="/" aria-label="7">
+                    <div class="screen7"></div>
+                </a>
+                <span class="cellTag">gallery</span>
             </div>
             <div class="centerCol">
-                <div class="screen8"></div>
+                <a href="/" aria-label="8">
+                    <div class="screen8"></div>
+                </a>
+                <span class="cellTag">contact</span>
             </div>
             <div class="rightCol">
-                <div class="screen9"></div>
+                <a href="/" aria-label="9">
+                    <div class="screen9"></div>
+                </a>
+                <span class="cellTag">about</span>
             </div>
         </div>
     </div>
---
+++ src/routes/about/+About.svelte
@@ -0,0 +1,27 @@
+<!-- grid layout                 -->
+<!-- PROJECT: lancesvelte;       -->
+<!-- (c) 2025 Lance Stubblefield -->
+
+
+
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="gridContainer">
+        <div class="gridCell">1</div>
+        <div class="gridCell">2</div>
+        <div class="gridCell">3</div>
+        <div class="gridCell">4</div>
+        <div class="gridCell">5</div>
+        <div class="gridCell">6</div>
+        <div class="gridCell">7</div>
+        <div class="gridCell">8</div>
+        <div class="gridCell">9</div>
+    </div>
+</main>
+
+<style>
+    @import 'about.css';
+</style>
