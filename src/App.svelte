8088b3e5d200baa7d6085908d4061ccfa20311ce
TimeSentinel <lance.stubblefield@gmail.com>
2025-01-26 18:05:50 -0800

    grid layout created

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,52 @@
+<!-- src/app.svelte              -->
+<!-- PROJECT: lancesvelte;       -->
+<!-- (c) 2025 Lance Stubblefield -->
+
+
 <script lang="ts">
 
 </script>
 
 <main>
-    <div>
-        Lance Stubblefield
+    <div class="container">
+        <div class="topRow">
+            <div class="leftCol">
+                <div class="screen1"></div>
+            </div>
+            <div class="centerCol">
+                <div class="screen2"></div>
+            </div>
+            <div class="rightCol">
+                <div class="screen3"></div>
+            </div>
+        </div>
+        <div class="middleRow">
+            <div class="leftCol">
+                <div class="screen4"></div>
+            </div>
+            <div class="mainFocus">
+                <div class="screen5">
+
+                </div>
+                <div class="focus">
+                    <h3>design by Lance</h3>
+                </div>
+            </div>
+            <div class="rightCol">
+                <div class="screen6"></div>
+            </div>
+        </div>
+        <div class="bottomRow">
+            <div class="leftCol">
+                <div class="screen7"></div>
+            </div>
+            <div class="centerCol">
+                <div class="screen8"></div>
+            </div>
+            <div class="rightCol">
+                <div class="screen9"></div>
+            </div>
+        </div>
     </div>
 </main>
 
-<style>
-
-</style>
